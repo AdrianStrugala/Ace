d3734069370398b134f75dd35f0d55e50463d5af
adrian-strugala <adrian.strugala@hpe.com>
2018-02-07 14:10:14 +0100

    problem with imports - check from home

--- src/main.py
+++ src/main.py
@@ -1,35 +1,8 @@
-import glob
 import os
-import sqlite3 as sql
+import repository
 import subprocess
 import winreg
 
-
-def create_table(conn):
-    sql_create_table = """ CREATE TABLE IF NOT EXISTS src (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        path text
-                                    ); """
-
-    try:
-        c = conn.cursor()
-        c.execute(sql_create_table)
-        print("Created table src")
-    except Exception as e:
-        print(e)
-
-
-def insert_row(conn, name, path):
-    sql_insert_row = """INSERT INTO src (name, path) VALUES (?, ?)"""
-
-    try:
-        c = conn.cursor()
-        c.execute(sql_insert_row, (name, path))
-        print("Inserted " + name)
-        print(c.lastrowid)
-    except Exception as e:
-        print(e)
 
 def subkeys(key):
     i = 0
@@ -41,62 +14,47 @@ def subkeys(key):
         except WindowsError as e:
             break
 
+
 if __name__ == '__main__':
-    with sql.connect('test.db') as conn:
-        create_table(conn)
 
+    repository.create_table()
 
-        keypath = r"Software\Microsoft\Windows\CurrentVersion\Uninstall"
-        aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+    key_path = r"Software\Microsoft\Windows\CurrentVersion\Uninstall"
+    aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
 
-        aKey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, keypath, 0, winreg.KEY_READ)
-        for subkeyname in subkeys(aKey):
+    aKey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_READ)
+    for sub_key_name in subkeys(aKey):
 
-            thiskey = keypath + "\\" + subkeyname
-            thisSubKey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, thiskey, 0, winreg.KEY_READ)
+        this_key = key_path + "\\" + sub_key_name
+        thisSubKey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, this_key, 0, winreg.KEY_READ)
 
-            path = ''
-            try:
-                path = winreg.QueryValueEx(thisSubKey, "InstallLocation")[0]
-            except:
-                path = ''
-            if path != '':
-                try:
-                    filepath = os.chdir(winreg.QueryValueEx(thisSubKey, "InstallLocation")[0])
-                    for file in glob.glob("*.exe"):
-                        print(file)
-
-                    insert_row(conn, winreg.QueryValueEx(thisSubKey, "DisplayName")[0], winreg.QueryValueEx(thisSubKey, filepath+ "\\" +file)[0])
-                except:
-                    None
-
-
-        c = conn.cursor()
-        c.execute("SELECT * FROM src")
-        rows = c.fetchall()
-
-        new_query = """SELECT path FROM src WHERE name LIKE ?"""
-
-
-
-        program = "%" + "Chrome" + "%"
-        c = conn.cursor()
-        c.execute(new_query, [program])
-
+        path = ''
         try:
-            record = c.fetchall()
+            path, regtype = winreg.QueryValueEx(thisSubKey, "InstallLocation")
+        except:
+            path = ''
+        if path != '':
+            try:
+                for file in os.listdir(path):
+                    if file.endswith(".exe"):
+                        fullpath = os.path.join(path, file)
+                        print(fullpath)
 
-            filepath = record[0]
-            print (filepath)
+                        repository.insert_program(winreg.QueryValueEx(thisSubKey, "DisplayName")[0], fullpath)
+            except:
+                None
 
-            os.chdir(filepath)
-            for file in glob.glob("*.exe"):
-                print(file)
+    program = "%" + "Chrome" + "%"
 
-            program = filepath + "\\" + file
-            print(program)
-            subprocess.Popen([program])
-        except Exception as e:
-            print(e)
+    #
+    #     subprocess.Popen([program])
+    # except Exception as e:
+    #     print(e)
 
-
+# os.chdir(filepath)
+# for file in glob.glob("*.exe"):
+#     print(file)
+#
+# program = filepath + "\\" + file
+# print(program)
+# except Exception as e:
---
+++ src/repository.py
@@ -0,0 +1,56 @@
+import sqlite3 as sql
+
+db = 'programs.db'
+
+
+def create_table():
+    with sql.connect(db) as conn:
+        sql_create_table = """ CREATE TABLE IF NOT EXISTS src (
+                                            id integer PRIMARY KEY,
+                                            name text NOT NULL,
+                                            path text
+                                        ); """
+
+        try:
+            c = conn.cursor()
+            c.execute(sql_create_table)
+            print("Created table " + db)
+        except Exception as e:
+            print(e)
+
+    conn.close()
+
+
+def insert_program(name, path):
+    with sql.connect(db) as conn:
+        sql_insert_row = """INSERT INTO src (name, path) VALUES (?, ?)"""
+
+        try:
+            c = conn.cursor()
+            c.execute(sql_insert_row, (name, path))
+            print("Inserted " + name)
+            print(c.lastrowid)
+        except Exception as e:
+            print(e)
+
+    conn.close()
+
+
+def get_program_path(name):
+    with sql.connect(db) as conn:
+        select_program = """SELECT path FROM src WHERE name LIKE ?"""
+
+        try:
+            c = conn.cursor()
+            c.execute(select_program, [name])
+            print("Inserted " + name)
+            print(c.lastrowid)
+
+
+            record = c.fetchall()
+
+            filepath = record[0]
+            print(filepath)
+
+        except Exception as e:
+            print(e)
