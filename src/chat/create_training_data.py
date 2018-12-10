2add94a65aaed57492fea4a4a3a95339eb859877
Adrian Strugala <adrian.strugala@comparex.com>
2018-12-10 12:02:37 +0100

    added interface reference - it should be now possible to use chatbot from main modult

--- src/chat/create_training_data.py
+++ src/chat/create_training_data.py
@@ -9,23 +9,23 @@ db = config['CHAT']['DB_NAME']
 batch_size = 1000
 
 def Execute():
-    
-    connection = sqlite3.connect(db)
-    last_unix = 0
-    current_length = batch_size
-    counter = 0
 
-    while current_length > 0:
-        dataframe = pandas.read_sql(f"""
-                                    SELECT * FROM parent_reply 
-                                    WHERE unix > {last_unix} and parent != ''
-                                    ORDER BY unix ASC
-                                    LIMIT {batch_size}
-                                    """, connection)
-        current_length = len(dataframe)
-        if current_length > 0:
-            last_unix = dataframe.tail(1)['unix'].values[0]
-            
+	connection = sqlite3.connect(db)
+	last_unix = 0
+	current_length = batch_size
+	counter = 0
+
+	while current_length > 0:
+		dataframe = pandas.read_sql(f"""
+									SELECT * FROM parent_reply 
+									WHERE unix > {last_unix} and parent != ''
+									ORDER BY unix ASC
+									LIMIT {batch_size}
+									""", connection)
+		current_length = len(dataframe)
+		if current_length > 0:
+			last_unix = dataframe.tail(1)['unix'].values[0]
+
 			with open("./chat/temp/train.from", 'a', encoding='utf8') as file:
 				for content in dataframe['parent'].values:
 					file.write(content + '\n')
@@ -33,6 +33,6 @@ def Execute():
 				for content in dataframe['comment'].values:
 					file.write(content + '\n')
 
-            counter +=1
-        if counter % 20 == 0:
-            print(str(counter * batch_size) + ' rows complited so far')
+			counter +=1
+		if counter % 20 == 0:
+			print(str(counter * batch_size) + ' rows complited so far')
--- src/main.py
+++ src/main.py
@@ -14,136 +14,134 @@ from programs import close_program
 from programs import add_program_manually
 from chat.AI_nmt.setup import prepare_data as nmt_prepare_data
 from chat.AI_nmt import train
+from chat.AI_nmt.inference import inference
 
 
 def sendMessage(text):
-    print(text)
-    speech.Say(text)
+	print(text)
+	speech.Say(text)
 
 
 def print_user_options():
-    print("1 - Create Database")
-    print("2 - Open Program")
-    print("3 - Close Program")
-    print("4 - Open Website")
-    print("5 - Search in Google")
-    print("6 - Clear Database")
-    print("7 - Manually add program to the Database")
-    print("8 - Teach me how to speak")
-    print("")
-    print("0 - Exit")
+	print("1 - Create Database")
+	print("2 - Open Program")
+	print("3 - Close Program")
+	print("4 - Open Website")
+	print("5 - Search in Google")
+	print("6 - Clear Database")
+	print("7 - Manually add program to the Database")
+	print("8 - Teach me how to speak")
+	print("")
+	print("0 - Exit")
 
 class switch(object):
-    value = None
+	value = None
 
-    def __new__(class_, value):
-        class_.value = value
-        return True
+	def __new__(class_, value):
+		class_.value = value
+		return True
 
 
 def case(*args):
-    return any((arg == switch.value for arg in args))
+	return any((arg == switch.value for arg in args))
 
 
 if __name__ == '__main__':
 
-    sendMessage("Hello. My name is Ace!")
+	sendMessage("Hello. My name is Ace!")
 
-    user_choice = -1
+	user_choice = -1
 
-    while user_choice != 0:
-        print("")
-        sendMessage("Make your choice")
-        print_user_options()
-        
-        try:
-            user_choice = int(input('Choice: '))
-        except ValueError:
-            sendMessage("Not a number")
+	while user_choice != 0:
+		print("")
+		sendMessage("Make your choice")
+		print_user_options()
 
-        while switch(user_choice):
-            if case(1):
-                sendMessage("Creating Database...")
-                try:
-                    create_programs_database.Execute()
-                except Exception as e:
-                    sendMessage("Cannot create database")
-                    print(e)
-                break
+		user_choice = input('Choice: ')
 
-            if case(2):
-                program_to_open = input('Type name of the program: ')
-                sendMessage("Opening Program... " + program_to_open)
-                try:
-                    open_program.Execute(program_to_open)
-                except Exception as e:
-                    sendMessage("Cannot open " + program_to_open)
-                    print(e)
-                break
+		while switch(user_choice):
+			if case(1):
+				sendMessage("Creating Database...")
+				try:
+					create_programs_database.Execute()
+				except Exception as e:
+					sendMessage("Cannot create database")
+					print(e)
+				break
 
-            if case(3):
-                program_to_close = input('Type name of the program: ')
-                sendMessage("Closing Program... " + program_to_close)
-                try:
-                    close_program.Execute(program_to_close)
-                except Exception as e:
-                    sendMessage("Cannot close" + program_to_close)
-                    print(e)
-                break
+			if case(2):
+				program_to_open = input('Type name of the program: ')
+				sendMessage("Opening Program... " + program_to_open)
+				try:
+					open_program.Execute(program_to_open)
+				except Exception as e:
+					sendMessage("Cannot open " + program_to_open)
+					print(e)
+				break
 
-            if case(4):
-                url_to_open = input('Write the url: ')
-                sendMessage("Opening website... " + url_to_open)
-                try:
-                    open_website.Execute(url_to_open)
-                except Exception as e:
-                    sendMessage("Cannot navigate to" + url_to_open)
-                    print(e)
-                break
+			if case(3):
+				program_to_close = input('Type name of the program: ')
+				sendMessage("Closing Program... " + program_to_close)
+				try:
+					close_program.Execute(program_to_close)
+				except Exception as e:
+					sendMessage("Cannot close" + program_to_close)
+					print(e)
+				break
 
-            if case(5):
-                phrase_to_search = input('What are you looking for: ')
-                sendMessage("Searching in Google for... " + phrase_to_search)
-                try:
-                    google_phrase.Execute(phrase_to_search)
-                except Exception as e:
-                    sendMessage("Cannot find " + phrase_to_search + " in Google")
-                    print(e)
-                break
+			if case(4):
+				url_to_open = input('Write the url: ')
+				sendMessage("Opening website... " + url_to_open)
+				try:
+					open_website.Execute(url_to_open)
+				except Exception as e:
+					sendMessage("Cannot navigate to" + url_to_open)
+					print(e)
+				break
 
-            if case(6):
-                sendMessage("Clearing database...")
-                try:
-                    clear_programs_database.Execute()
-                except Exception as e:
-                    sendMessage("Cannot clear database")
-                    print(e)
-                break
+			if case(5):
+				phrase_to_search = input('What are you looking for: ')
+				sendMessage("Searching in Google for... " + phrase_to_search)
+				try:
+					google_phrase.Execute(phrase_to_search)
+				except Exception as e:
+					sendMessage("Cannot find " + phrase_to_search + " in Google")
+					print(e)
+				break
 
-            if case(7):
-                sendMessage("Adding program...")
-                try:
-                    add_program_manually.Execute()
-                except Exception as e:
-                    sendMessage("Cannot add program")
-                    print(e)
-                break
+			if case(6):
+				sendMessage("Clearing database...")
+				try:
+					clear_programs_database.Execute()
+				except Exception as e:
+					sendMessage("Cannot clear database")
+					print(e)
+				break
 
-            if case(8):
-                sendMessage("Learning how to speak...")
-                try:
-                 #   create_database.Execute()
-                 #  create_training_data.Execute()
-                    nmt_prepare_data.Execute()
-                    train.Execute()
-                except Exception as e:
-                    sendMessage("I still cannot speak :(")
-                    print(e)
-                break
+			if case(7):
+				sendMessage("Adding program...")
+				try:
+					add_program_manually.Execute()
+				except Exception as e:
+					sendMessage("Cannot add program")
+					print(e)
+				break
 
-            if case(0):
-                sendMessage("Exiting")
-                break
+			if case(8):
+				sendMessage("Learning how to speak...")
+				try:
+				 #   create_database.Execute()
+				 #  create_training_data.Execute()
+					nmt_prepare_data.Execute()
+					train.Execute()
+				except Exception as e:
+					sendMessage("I still cannot speak :(")
+					print(e)
+				break
 
-            sendMessage("Unrecognized option.")
-            break
+			if case(0):
+				sendMessage("Exiting")
+				break
+
+			print(inference(user_choice))
+			break
