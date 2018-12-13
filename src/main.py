b6ab81869b7fe3393403ac74fbdc76a23876cecb
Adrian Strugala <strugala.adrian@gmail.com>
2018-12-13 20:41:49 +0100

    removed run in parallel separate method

--- src/main.py
+++ src/main.py
@@ -9,23 +9,7 @@ from user_interface import console
 
 
 
-
-
-def runInParallel(*fns):
-    with Manager() as manager:
-        list_to_say = manager.list()
-        xd = sys.stdin.fileno()  # get original file descriptor
-        processes = []
-        for fn in fns:
-            p = Process(target=fn, args=(list_to_say,xd,))
-            p.start()
-            processes.append(p)
-        for p in processes:
-            p.join()
-
-
 if __name__ == '__main__':
-   # runInParallel(speech.Run, console.display_menu)
     with Manager() as manager:
         list_to_say = manager.list()
         stdin = sys.stdin.fileno()  # get original file descriptor
